a518cca011e5b1770a71ffbad159f287273f5af3
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-03-21 16:20:01 +0100

    Your commit message

--- client/src/Layout/AppLayout.vue
+++ client/src/Layout/AppLayout.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="app-layout">
     <header>
-      <div v-if="isLoggedIn()" class="logo">
-        enklare<span class="dot">.</span>
-      </div>
+      <div class="logo">Agent<span class="logoX">X</span></div>
       <!-- header contents -->
       <nav class="nav-links">
         <router-link v-if="!isLoggedIn()" class="router-link" to="/register"
@@ -31,108 +29,107 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { useStore } from "vuex";
-import api from "../services/api";
-import { useRouter } from "vue-router";
+  import { defineComponent } from "vue";
+  import { useStore } from "vuex";
+  import api from "../services/api";
+  import { useRouter } from "vue-router";
 
-export default defineComponent({
-  name: "AppLayout",
-  setup() {
-    const store = useStore();
-    const router = useRouter();
-    const isLoggedIn = () => store.state.userLoggedIn;
+  export default defineComponent({
+    name: "AppLayout",
+    setup() {
+      const store = useStore();
+      const router = useRouter();
+      const isLoggedIn = () => store.state.userLoggedIn;
 
-    const handleLogout = () => {
-      // Clear user data from the local storage and Vuex store
-      localStorage.removeItem("user");
-      localStorage.removeItem("firstName");
-      localStorage.removeItem("token");
-      store.commit("setUserLoggedIn", false);
-      store.commit("setFirstName", "");
+      const handleLogout = () => {
+        // Clear user data from the local storage and Vuex store
+        localStorage.removeItem("user");
+        localStorage.removeItem("firstName");
+        localStorage.removeItem("token");
+        store.commit("setUserLoggedIn", false);
+        store.commit("setFirstName", "");
 
-      // Remove the Authorization header from the axios instance
-      delete api.defaults.headers.common["Authorization"];
+        // Remove the Authorization header from the axios instance
+        delete api.defaults.headers.common["Authorization"];
 
-      // Redirect to the login page
-      router.push("/login");
-    };
+        // Redirect to the login page
+        router.push("/login");
+      };
 
-    return {
-      isLoggedIn,
-      handleLogout,
-    };
-  },
-});
+      return {
+        isLoggedIn,
+        handleLogout,
+      };
+    },
+  });
 </script>
 
 <style scoped>
-.app-layout {
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  min-height: 100vh;
+  .app-layout {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
 
-  margin: 0 auto;
-}
-
-header {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  background-color: rgb(30 35 35 / 0.8);
-  padding: 1rem;
-  z-index: 99;
-}
-
-.logo {
-  flex-direction: row;
-  position: absolute;
-  left: 4%;
-  font-size: 1.2rem;
-  font-weight: 800;
-  color: rgb(255, 255, 255);
-}
-
-.dot {
-  font-size: 1.3rem;
-  padding: 0.6px;
-  color: rgb(42, 187, 37);
-}
-
-.se {
-  margin-top: 0.18rem;
-  font-size: 1rem;
-}
-
-.nav-links {
-  display: flex;
-  gap: 1rem;
-  margin-right: 2%;
-}
-
-.router-link-exact-active,
-.router-link-active,
-.router-link {
-  margin-top: 3px;
-  background-color: #1c3b69;
-  text-decoration: none;
-  border: none;
-  font-size: 10pt;
-  font-weight: 600;
-  color: white;
-  padding: 0.5rem 0.9rem;
-
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-main {
-  padding: 1rem;
-}
-
-@media (max-width: 768px) {
-  main {
-    padding: 1rem 0.5rem;
+    margin: 0 auto;
   }
-} ;
+
+  header {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: rgb(30 35 35 / 0.8);
+    padding: 1rem;
+    z-index: 99;
+  }
+
+  .logo {
+    flex-direction: row;
+    position: absolute;
+    left: 4%;
+    font-size: 1.2rem;
+    font-weight: 800;
+    color: rgb(255 255 255 / 0.9);
+  }
+  .logoX {
+    font-size: 1.1rem;
+    color: rgba(75, 140, 237, 0.8);
+    margin-left: 2px;
+  }
+
+  .se {
+    margin-top: 0.18rem;
+    font-size: 1rem;
+  }
+
+  .nav-links {
+    display: flex;
+    gap: 1rem;
+    margin-right: 2%;
+  }
+
+  .router-link-exact-active,
+  .router-link-active,
+  .router-link {
+    margin-top: 3px;
+    background-color: #1c3b69;
+    text-decoration: none;
+    border: none;
+    font-size: 10pt;
+    font-weight: 600;
+    color: white;
+    padding: 0.5rem 0.9rem;
+
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  main {
+    padding: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    main {
+      padding: 1rem 0.5rem;
+    }
+  } ;
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,68 +1,70 @@
 <template>
   <div class="home-container">
     <div class="welcome-message">
-      <h1>Save money big, <span>fast & free</span></h1>
-      <h2>Collect and compare loans up to SEK 600,000</h2>
-      <button>Start your application</button>
+      <h1>Boost your Productivity with <span>AI</span></h1>
+      <h2>
+        Slash Your Enterprise's HR and Customer Service Costs by up to 60%
+      </h2>
+      <button>See how</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+  import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: "Home",
-});
+  export default defineComponent({
+    name: "Home",
+  });
 </script>
 
 <style scoped>
-.home-container {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-    url("../assets/bg.jpeg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  .home-container {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+      url("../assets/bgAi.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-.welcome-message {
-  text-align: center;
-  color: #fff;
-}
+  .welcome-message {
+    text-align: center;
+    color: #fff;
+  }
 
-h1 {
-  font-size: 19pt;
-  font-weight: 700;
-}
+  h1 {
+    font-size: 19pt;
+    font-weight: 700;
+  }
 
-span {
-  color: #3bf172;
-}
+  span {
+    color: #3bf172;
+  }
 
-h2 {
-  font-size: 11pt;
-  opacity: 0.8;
-  font-weight: 500;
-}
+  h2 {
+    font-size: 11pt;
+    opacity: 0.8;
+    font-weight: 500;
+  }
 
-button {
-  margin-top: 20px;
-  background-color: #1c9a42;
-  text-decoration: none;
-  border: none;
-  font-size: 11pt;
-  font-weight: 600;
-  color: white;
-  padding: 0.5rem 1.4rem;
-  border-radius: 4px;
-  cursor: pointer;
-}
+  button {
+    margin-top: 20px;
+    background-color: #1c9a42;
+    text-decoration: none;
+    border: none;
+    font-size: 11pt;
+    font-weight: 600;
+    color: white;
+    padding: 0.5rem 1.4rem;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 </style>
